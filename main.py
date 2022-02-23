1942f11886dc643be4daa363a049d89e4e059c15
roguetaver <andretavernaro@gmail.com>
2022-02-23 17:13:32 -0300

    comeco do roteiro 1 pt 2

--- main.py
+++ main.py
@@ -3,25 +3,71 @@ import sys
 
 class Token:
     def __init__(self, type, value):
-        self.type = type
-        self.value = value
+        self.type = type  # tipo do token
+        self.value = value  # valor do token
 
 
 class Tokenizer:
     def __init__(self, origin, position, actual):
-        self.origin = origin
-        self.position = position
-        self.actual = actual
+        self.origin = origin  # codigo fonte que sera tokenizado
+        self.position = position  # posição atual que o tokenizador está separando
+        self.actual = actual  # o ultimo token separando
 
     def selectNext():
-        print("le o proximo token e atualiza o atributo atual")
+        # le o proximo token e atualiza o atributo atual
+        '''
+        se origin[position] == '+' 
+            position ++
+            actual  = Token('','plus')
+            return actual
+        se origin[position] == '-'
+            actual = Token('','minus')
+            return actual
+        se position >= len(origin)
+            actual = token('','eof')
+        se origin[position] is numeric
+            candidato = origin[position]
+            position ++
+            enquanto origin [position] is numeric
+                candidato = origin[position]
+                position++
+        else raise error
+
+        [TRATAR ESPAÇOS]
+        '''
+        print("hello")
 
 
 class Parser:
-    token = None
+    tokens = None  # objeto da classe que era ler o codigo fonte e alimentar o analisador
 
     def parseExpression():
-        print("consome os tokens do tokenizer e analisa se a sintaze esta aderente a gramatica proposta retorna o resultado da expressão analisada")
+        # consome os tokens do tokenizer e analisa se a sintaze esta aderente
+        # a gramatica proposta retorna o resultado da expressão analisada
+        '''
+        Se o token atual for número:
+        Copiar número para o resultado
+        Pegar próximo token
+        Enquanto token for + ou -:
+        Se o token atual é +:
+        Pegar próximo token
+        Se o token atual for número:
+        Somar o número no resultado
+        Senão ERRO
+        Se o token atual é -:
+        Pegar próximo token
+        Se o token atual for número:
+        Subtrair o número no resultado
+        Senão ERRO
+        Pegar próximo token
+        Retornar resultado
+        Senão ERRO
+        '''
+
+        print("hello")
 
     def run(code):
-        print("receve o codigo fonte como argumento, inicializa im objeto tokenizador e retorna o resultado do parse expression(). Esse metodo serpa chamado pelo main()")
+        # receve o codigo fonte como argumento, inicializa um objeto tokenizador e
+        # retorna o resultado do parse expression(). Esse metodo serpa chamado pelo main()
+
+        print("hello")
