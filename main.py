78466b6d22ebd893b837b34edcd7fc5ba07cda62
roguetaver <andretavernaro@gmail.com>
2022-03-09 18:29:20 -0300

    bug fix 13 roteiro 2

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ class Parser:
     def parseTerm():
         # consome os tokens do tokenizer e analisa se a sintaze esta aderente
         # a gramatica proposta retorna o resultado da expressão analisada
-        Parser.tokens.selectNext()
+
         while(Parser.tokens.actual.type != "EOF"):
             if(Parser.tokens.actual.type == "numeric"):
 
