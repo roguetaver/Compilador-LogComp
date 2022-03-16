a2ae98c9316bc07bbfb90403dbee13671a3cb44b
roguetaver <andretavernaro@gmail.com>
2022-03-16 17:36:54 -0300

    first try

--- main.py
+++ main.py
@@ -52,6 +52,16 @@ class Tokenizer:
             self.actual = Token("div", 0)
             return self.actual
 
+        elif(self.origin[self.position] == '('):
+            self.position += 1
+            self.actual = Token("openParentheses", 0)
+            return self.actual
+
+        elif(self.origin[self.position] == ')'):
+            self.position += 1
+            self.actual = Token("closeParentheses", 0)
+            return self.actual
+
         elif(self.origin[self.position].isnumeric()):
             cadidato = self.origin[self.position]
             self.position += 1
@@ -72,67 +82,68 @@ class Parser:
     tokens = None  # objeto da classe que era ler o codigo fonte e alimentar o analisador
 
     @staticmethod
-    def parseTerm():
+    def parseFactor():
         # consome os tokens do tokenizer e analisa se a sintaze esta aderente
         # a gramatica proposta retorna o resultado da expressão analisada
 
-        while(Parser.tokens.actual.type != "EOF"):
-            if(Parser.tokens.actual.type == "numeric"):
+        if(Parser.tokens.actual.type == "numeric"):
+            resultado = Parser.tokens.actual.value
+            Parser.tokens.selectNext()
 
-                resultado = Parser.tokens.actual.value
+        elif(Parser.tokens.actual.type == "minus"):
+            Parser.tokens.selectNext()
+            resultado = -Parser.parseFactor()
+
+        elif(Parser.tokens.actual.type == "plus"):
+            Parser.tokens.selectNext()
+            resultado = Parser.parseFactor()
+
+        elif(Parser.tokens.actual.type == "openParentheses"):
+            Parser.tokens.selectNext()
+            resultado = Parser.parseExpression()
+            if(Parser.tokens.actual.type == "closeParentheses"):
                 Parser.tokens.selectNext()
-
-                if(Parser.tokens.actual.type == "numeric"):
-                    raise ValueError("ERROR")
-
-                while(Parser.tokens.actual.type == "mult" or Parser.tokens.actual.type == "div"):
-
-                    if(Parser.tokens.actual.type == "mult"):
-                        Parser.tokens.selectNext()
-                        if(Parser.tokens.actual.type == "numeric"):
-                            resultado *= Parser.tokens.actual.value
-                        else:
-                            raise ValueError("ERROR")
-
-                    elif(Parser.tokens.actual.type == "div"):
-                        Parser.tokens.selectNext()
-                        if(Parser.tokens.actual.type == "numeric"):
-                            resultado /= Parser.tokens.actual.value
-                        else:
-                            raise ValueError("ERROR")
-
-                    Parser.tokens.selectNext()
-
-                return resultado
-
             else:
                 raise ValueError("ERROR")
+
+        else:
+            raise ValueError("ERROR")
+
+        return resultado
+
+    @staticmethod
+    def parseTerm():
+       # consome os tokens do tokenizer e analisa se a sintaze esta aderente
+        # a gramatica proposta retorna o resultado da expressão analisada
+        resultado = Parser.parseFactor()
+
+        while((Parser.tokens.actual.type == "mult" or Parser.tokens.actual.type == "div")):
+
+            if(Parser.tokens.actual.type == "mult"):
+                Parser.tokens.selectNext()
+                resultado *= Parser.parseFactor()
+
+            elif(Parser.tokens.actual.type == "div"):
+                Parser.tokens.selectNext()
+                resultado //= Parser.parseFactor()
+
+        return resultado
 
     @staticmethod
     def parseExpression():
         # consome os tokens do tokenizer e analisa se a sintaze esta aderente
         # a gramatica proposta retorna o resultado da expressão analisada
-        Parser.tokens.selectNext()
         resultado = Parser.parseTerm()
 
-        if(Parser.tokens.actual.type == "numeric"):
-            raise ValueError("ERROR")
-
-        while((Parser.tokens.actual.type == "minus" or Parser.tokens.actual.type == "plus") and Parser.tokens.actual.type != "EOF"):
+        while((Parser.tokens.actual.type == "minus" or Parser.tokens.actual.type == "plus")):
 
             if(Parser.tokens.actual.type == "minus"):
                 Parser.tokens.selectNext()
-                if(Parser.tokens.actual.type == "numeric"):
-                    resultado -= Parser.parseTerm()
-                else:
-                    raise ValueError("ERROR")
+                resultado -= Parser.parseTerm()
 
             elif(Parser.tokens.actual.type == "plus"):
                 Parser.tokens.selectNext()
-                if(Parser.tokens.actual.type == "numeric"):
-                    resultado += Parser.parseTerm()
-                else:
-                    raise ValueError("ERROR")
+                resultado += Parser.parseTerm()
 
         return resultado
 
@@ -141,12 +152,16 @@ class Parser:
         # retorna o resultado do parse expression(). Esse metodo sera chamado pelo main()
         postProCode = PrePro.filter(code)
         Parser.tokens = Tokenizer(postProCode)
-        print(int(Parser.parseExpression()))
-        return Parser.parseExpression()
+        Parser.tokens.selectNext()
+
+        result = Parser.parseExpression()
+        if(Parser.tokens.actual.type != "EOF"):
+            raise ValueError("ERROR")
+        return result
 
 
 if(len(sys.argv) <= 1):
     raise ValueError("ERROR")
 
 arg = str(sys.argv[1])
-Parser.run(arg)
+print(Parser.run(arg))
